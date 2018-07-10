687b9b2791d472bdfae1d2273dc016c137f89362
Daniel <ddazaj@uninorte.edu.co>
2018-07-10 00:22:55 -0500

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+// Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+// Initialize
+const app = express();
+const users = require("./routes/users.js");
+const posts = require("./routes/posts.js");
+const profile = require("./routes/profile.js");
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
+
+// DB Initialize
+const db = require("./config/database").mongoURI;
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log(`Connected to the database`))
+  .catch(err => console.log(err));
+
+// Start Server
+const port = process.env.Port || 8000;
+app.listen(port, () => console.log(`Server start in port ${port}`));
